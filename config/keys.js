315607cc645960ce29788a0647e46ca9c307db2e
Pawel Galkowski <poczta@itgalkowski.pl>
2023-03-21 19:32:04 +0100

    add config

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+﻿require('dotenv').config()
+
+module.exports = {
+  mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@chat-vorap.mongodb.net/test?retryWrites=true&w=majority`,
+  secretOrKey: process.env.MONGO_SERCRET,
+  options: {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+}
---
+++ config/passport.js
@@ -0,0 +1,20 @@
+﻿const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const mongoose = require('mongoose')
+const User = mongoose.model('users')
+require('dotenv').config()
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(opts, (jwtPayload, done) => {
+      User.findById(jwtPayload.id)
+        .then((user) => done(null, user || false))
+        .catch((err) => console.error(err))
+    })
+  )
+}
